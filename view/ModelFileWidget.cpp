f3a1a0da0fe0f9e201bd8f4a6449ecceffbea31a
unbusy <unbusy@lhost.com>
2020-07-07 00:09:15 +0200

    Cleanup

--- view/ModelFileWidget.cpp
+++ view/ModelFileWidget.cpp
@@ -28,15 +28,15 @@
 #include <cstdlib>
 #include <filesystem>
 
-  #include <boost/archive/tmpdir.hpp>
+#include <boost/archive/tmpdir.hpp>
 
-  #include <boost/archive/text_iarchive.hpp>
-  #include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
 
-  #include <boost/serialization/base_object.hpp>
-  #include <boost/serialization/utility.hpp>
-  #include <boost/serialization/map.hpp>
-  #include <boost/serialization/assume_abstract.hpp>
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/utility.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/assume_abstract.hpp>
 
 using namespace Fxt;
 namespace fs = std::filesystem;
