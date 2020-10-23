4ceeaf04f46bc35880e2d587aa8ea7a628093559
Wilfred Dube <wilfreddube@gmail.com>
2020-10-23 09:49:05 +0200

    Add point class

---
+++ libfxtract/include/sheet-metal-component/Point.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "BoostSerializer.h"
+#include <ostream>
+
+namespace Fxt 
+{
+namespace SheetMetalComponent
+{
+    class Point 
+    {
+        friend class boost::serialization::access;
+        friend std::ostream & operator<<(std::ostream &os, const Point &p);
+
+        template<class Archive>
+        void serialize(Archive &ar, const unsigned int version)
+        {
+            // save/load base class information
+            ar & X & Y & Z;
+        }
+
+    public:
+        long double X, Y, Z;
+  };
+}
+
+}
