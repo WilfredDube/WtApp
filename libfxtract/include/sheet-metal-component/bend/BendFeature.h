5fe0a2c2efdf820def6822352a9de53f1c888430
Wilfred Dube <wilfreddube@gmail.com>
2020-10-23 09:52:05 +0200

    Add bend feature class

---
+++ libfxtract/include/sheet-metal-component/bend/BendFeature.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "../BoostSerializer.h"
+#include "../edge/ModelEdge.h"
+#include "../MFaceAbstract.h"
+#include "../Point.h"
+#include "../../Computation.h"
+
+// #include "BendComputation.h"
+
+#include <gp_Lin.hxx>
+#include <TopoDS_Face.hxx>
+#include <vector>
+
+// class ModelBend;
+
+namespace Fxt
+{
+namespace SheetMetalComponent
+{
+namespace Bend
+{
+    class BendFeature
+    {
+        using FaceID = Fxt::SheetMetalComponent::ModelTypes::FaceID;
+
+        long double mBendLength;
+        long double mCurvature; //!< face curvature value : zero for planar type faces and non-zero for non-planar type faces.
+
+        //!< the face ID of the face connected to the bend face via its Straight edge.
+        FaceID mJoinedFaceId1;
+
+        //!< the face ID of the second face connected to the bend face via its Straight edge.
+        FaceID mJoinedFaceId2;
+
+        long double mBendingForce; //!< force required to create the bend.
+        long double mBendAngle; //!< bend angle.
+        long double mBendRadius; //!< bend radius : zero if face is planar and non-zero if face is a bend.
+        size_t mBendDirection;
+
+        // std::shared_ptr<BendComputation> mBendComputation;
+    public:
+
+        void setJoiningFaceID1(const FaceID mFaceID);
+        FaceID getJoiningFaceID1() const;
+
+        void setJoiningFaceID2(const FaceID mFaceID);
+        FaceID getJoiningFaceID2() const;
+
+        void setBendAngle(const long double angle);
+        long double getBendAngle() const;
+
+        void setBendDirection(const size_t bend_direction);
+        size_t getBendDirection() const;
+
+        void setBendLength(const long double bendLength);
+        long double getBendLength() const;
+
+        void setCurvature(const long double cv);
+        long double getBendRadius() const;
+        
+        // TODO : calculate Bending force, direction, orientation & distance
+    };
+}
+}
+}
---
+++ libfxtract/src/sheet-metal-component/bend/BendFeature.cpp
@@ -0,0 +1,69 @@
+#include "../../../include/sheet-metal-component/bend/BendFeature.h"
+#include "../../../include/sheet-metal-component/bend/ModelBend.h"
+
+using namespace Fxt::SheetMetalComponent::Bend;
+
+void BendFeature::setJoiningFaceID1(const FaceID mFaceID)
+{
+    mJoinedFaceId1 = mFaceID;
+}
+
+FaceID BendFeature::getJoiningFaceID1() const 
+{ 
+    return mJoinedFaceId1; 
+}
+
+void BendFeature::setJoiningFaceID2(FaceID mFaceID)
+{
+    mJoinedFaceId2 = mFaceID;
+}
+
+FaceID BendFeature::getJoiningFaceID2() const
+{
+    return mJoinedFaceId2;
+}
+
+void BendFeature::setBendAngle(const long double angle)
+{
+    mBendAngle = angle;
+}
+
+long double BendFeature::getBendAngle() const
+{
+    return mBendAngle;
+}
+
+void BendFeature::setBendDirection(const size_t bend_direction)
+{
+    mBendDirection = bend_direction;
+}
+
+size_t BendFeature::getBendDirection() const
+{
+    return mBendDirection;
+}
+
+void BendFeature::setBendLength(const long double bendLength)
+{
+    mBendLength = bendLength;
+}
+
+long double BendFeature::getBendLength() const
+{
+    return mBendLength;
+}
+
+void BendFeature::setCurvature(const long double cv)
+{
+    mCurvature = cv;
+
+    if (mCurvature != 0.0) {
+      // Divide by 2 to get radius else you get the diameter.
+      mBendRadius = roundd(((mCurvature < 0.0) ? -(1 / mCurvature) : (1 / mCurvature)) / 2);
+    }
+}
+
+long double BendFeature::getBendRadius() const
+{
+    return mBendRadius;
+}
