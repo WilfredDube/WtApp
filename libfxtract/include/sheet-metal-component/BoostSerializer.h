6d9a9fc00332ec82184ceba382cd28f217037bc2
Wilfred Dube <wilfreddube@gmail.com>
2020-10-23 09:46:24 +0200

    Add Serialization headers

---
+++ libfxtract/include/sheet-metal-component/BoostSerializer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <boost/archive/tmpdir.hpp>
+
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/utility.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/assume_abstract.hpp>
