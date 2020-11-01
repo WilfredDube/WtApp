9dda13ef2b9db8c0c6f2edbd53ad5e0ef80a94d6
Wilfred Dube <wilfreddube@gmail.com>
2020-11-01 19:21:42 +0200

    Implement method to calculate the bend line

---
+++ libfxtract/src/sheet-metal-component/bend/BendLine.cpp
@@ -0,0 +1,44 @@
+#include "../../../include/sheet-metal-component/bend/BendLine.h"
+#include "../../../include/Computation.h"
+#include "../../../include/sheet-metal-component/Point.h"
+#include "../../../include/sheet-metal-component/edge/ModelEdge.h"
+
+#include <gp_Pnt.hxx>
+
+using namespace Fxt::SheetMetalComponent::Bend;
+using namespace Fxt::SheetMetalComponent::Edge;
+
+void BendLine::computeBendLine(
+    const std::vector<std::shared_ptr<Fxt::SheetMetalComponent::Edge::ModelEdge>>& arcEdges)
+{
+    if(arcEdges.empty() || arcEdges.size() != 2)
+        return; // error 
+    
+    std::vector<gp_Pnt> endPoints;
+
+    endPoints.push_back(computeMidPoint(arcEdges[0]->getEdgeEndPoints()[0], 
+                                arcEdges[0]->getEdgeEndPoints()[1])
+                        );
+
+    endPoints.push_back(computeMidPoint(arcEdges[1]->getEdgeEndPoints()[0], 
+                                arcEdges[1]->getEdgeEndPoints()[1])
+                        );
+
+    gp_Pnt dirVertex(
+      endPoints[0].X() - endPoints[1].X(),
+      endPoints[0].Y() - endPoints[1].Y(),
+      endPoints[0].Z() - endPoints[1].Z()
+    );
+
+
+    mBendLine = std::make_shared<gp_Lin>(endPoints[0], gp_Dir(dirVertex.X(), dirVertex.Y(), dirVertex.Z()));
+
+    mBendLinePnt->X = endPoints[0].X();//bendLine_.Location().X();
+    mBendLinePnt->Y = endPoints[0].Y();//bendLine_.Location().Y();
+    mBendLinePnt->Z = endPoints[0].Z();//bendLine_.Location().Z();
+
+    mBendLineDir->X = dirVertex.X();
+    mBendLineDir->Y = dirVertex.Y();
+    mBendLineDir->Z = dirVertex.Z();
+}
+
