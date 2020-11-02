01a309fecce94d05100f3dc6b7164a87921dddcf
Wilfred Dube <wilfreddube@gmail.com>
2020-11-02 12:10:58 +0200

    Add Class to hold sheet metal components

---
+++ libfxtract/include/sheet-metal-component/SheetMetalFeature.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "ModelTypes.h"
+
+#include <TopoDS_Shape.hxx>
+
+#include <map>
+#include <memory>
+
+namespace Fxt
+{
+namespace SheetMetalComponent
+{
+    namespace Face
+    {
+        class ModelFace;
+    }
+
+    namespace Bend
+    {
+        class ModelBend;
+    }
+
+    using FaceID = Fxt::SheetMetalComponent::ModelTypes::FaceID;
+
+    class SheetMetalFeature
+    {
+        std::map<FaceID, std::shared_ptr<Face::ModelFace>> mModelFaces;
+        std::map<FaceID, std::shared_ptr<Bend::ModelBend>> mModelBends;
+
+        long double mThickness;
+
+        std::shared_ptr<TopoDS_Shape> mTopologicalShape;
+
+    public:
+        void addModelFace(std::shared_ptr<Face::ModelFace>& modelFace);
+
+        void addModelBend(std::shared_ptr<Bend::ModelBend>& modelBend);
+
+        std::shared_ptr<gp_Dir> getNormalByFaceID(const FaceID faceID) const;
+
+        std::shared_ptr<Bend::ModelBend> getBendWithJoiningFaceID(const FaceID faceID) const;
+        
+        bool findFace(FaceID id, std::map<FaceID, std::shared_ptr<Bend::ModelBend>>& BL, 
+                        std::map<FaceID, std::shared_ptr<Bend::ModelBend>>& innerBends);
+
+        std::map<FaceID, std::shared_ptr<Face::ModelFace>> getFaces() const;
+        std::map<FaceID, std::shared_ptr<Bend::ModelBend>> getBends() const;
+
+        double getThickness() const;
+
+        void assignFaceAttributes(FaceID faceID, TopoDS_Shape& aShape);
+
+        void classifyFaces();
+
+        void computeBendAngles();
+
+        bool cleanModel();
+    };
+}
+}
