5a72daa179abd4476d11801ab783e7548c64de05
Wilfred Dube <wilfreddube@gmail.com>
2021-01-13 09:47:09 +0200

    Add Null object : avoid throwing exception when CadFileReader is not successfully created

---
+++ libfxtract/include/cad-file-reader/NullFileReader.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CadFileReader.h"
+
+namespace Fxt
+{
+namespace CadFileReader
+{    
+    class NullFileReader : public Fxt::CadFileReader::CadFileReader 
+    {
+    public:
+        void makeReader(const std::string& filename) {}
+        void extractFaces(SheetMetalFeaturePtr& model, const std::string& filename){}
+        bool isUsable() const noexcept override { return false; }
+    };
+}
+}
