6cdd60b3c0f05235fef82db31ba3b768b04330f0
Wilfred Dube <wilfreddube@gmail.com>
2021-01-13 09:54:35 +0200

    Add helper method for creating a data time string

--- logging/include/LoggingFacility.h
+++ logging/include/LoggingFacility.h
@@ -16,6 +16,17 @@ namespace Logging
         virtual void writeWarnEntry(std::string_view username, std::string_view entry, std::string_view other = "") = 0;
         virtual void writeErrorEntry(std::string_view username, std::string_view entry, std::string_view other = "") = 0;
 
+    private:
+        inline std::string dateTimeString() noexcept
+        {
+            std::time_t t_t = time(nullptr); // Get current system time
+            std::tm* t = localtime(&t_t); // Convert to local tim
+
+            char buffer[256];
+            strftime(buffer, sizeof(buffer), "%c", t);
+
+            return buffer;
+        }
     };
 }
 }
