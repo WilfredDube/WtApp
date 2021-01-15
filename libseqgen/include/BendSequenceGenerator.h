273f36778c7a7d7774ffbd8084c7ec7541eb4c76
Wilfred Dube <wilfreddube@gmail.com>
2021-01-15 14:33:35 +0200

    Add bend sequence module

---
+++ libseqgen/include/BendSequenceGenerator.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../../libfxtract/include/sheet-metal-component/SheetMetalFeature.h"
+
+#include <memory>
+#include <vector>
+
+class BendSequenceGenerator
+{
+    struct Sequence
+    {
+        std::vector<int> chromosome;
+        unsigned nTools{1}, nRotations{0}, nFlips{0}, targetSize;
+        double distance{0}, fitness;
+
+        Sequence(std::vector<int> chromosome) : chromosome { chromosome } {}
+
+        double cal_fitness(Fxt::SheetMetalComponent::SheetMetalFeature& model);
+        
+        Sequence mate(Sequence parentSequence);
+        bool operator<(const Sequence &individualSequence);
+        bool operator==(const Sequence& individualSequence);
+    };
+
+    std::shared_ptr<Sequence> sequenceImpl_;
+
+    void print(const std::vector<Sequence>& new_generation);
+
+public:
+    BendSequenceGenerator() = default;
+    BendSequenceGenerator(std::vector<int> chromosome, Fxt::SheetMetalComponent::SheetMetalFeature& model);
+
+    Sequence generateBendingSequence();
+};
