f9f76e21a86920e29416e0479b827c53e37f1fae
Wilfred Dube <wilfreddube@gmail.com>
2021-01-18 22:55:28 +0200

    Usage of ToolDao

--- model/src/Session.cpp
+++ model/src/Session.cpp
@@ -11,6 +11,7 @@
 #include "../include/Material.h"
 
 #include "../../dbdao/include/UserDao.h"
+#include "../../dbdao/include/ToolDao.h"
 
 #include <Wt/Auth/AuthService.h>
 #include <Wt/Auth/HashFunction.h>
@@ -147,80 +148,23 @@ void Session::fillMaterial()
 
 void Session::fillTools()
 {
-  dbo::ptr<Tool> MFA(Wt::cpp14::make_unique<Tool>());
-  Tool *a = MFA.modify();
-  a->addTool("AC26", "ACUTE", 26, 1000, 0.2, 50);
-  add(MFA);
+  ToolDao toolDao{*this};
 
-  dbo::ptr<Tool> MFB(Wt::cpp14::make_unique<Tool>());
-  Tool *b = MFB.modify();
-  b->addTool("AC30", "ACUTE", 30, 1000, 0.2, 50);
-  add(MFB);
-
-  dbo::ptr<Tool> MFC(Wt::cpp14::make_unique<Tool>());
-  Tool *c = MFC.modify();
-  c->addTool("AC35", "ACUTE", 35, 1000, 0.2, 50);
-  add(MFC);
-
-  dbo::ptr<Tool> MFD(Wt::cpp14::make_unique<Tool>());
-  Tool *d = MFD.modify();
-  d->addTool("AC45", "ACUTE", 45, 1000, 0.2, 50);
-  add(MFD);
-
-  dbo::ptr<Tool> MFE(Wt::cpp14::make_unique<Tool>());
-  Tool *e = MFE.modify();
-  e->addTool("AC60", "ACUTE", 60, 1000, 0.2, 50);
-  add(MFE);
-
-  dbo::ptr<Tool> MFF(Wt::cpp14::make_unique<Tool>());
-  Tool *f = MFF.modify();
-  f->addTool("AC88", "GOOSENECK", 88, 1000, 0.2, 50);
-  add(MFF);
-
-  dbo::ptr<Tool> MFG(Wt::cpp14::make_unique<Tool>());
-  Tool *g = MFG.modify();
-  g->addTool("AC90", "GOOSENECK", 90, 1000, 0.2, 50);
-  add(MFG);
-
-  dbo::ptr<Tool> MFH(Wt::cpp14::make_unique<Tool>());
-  Tool *h = MFH.modify();
-  h->addTool("VT15", "VTOOL", 15, 1000, 0.2, 50);
-  add(MFH);
-
-  dbo::ptr<Tool> MFI(Wt::cpp14::make_unique<Tool>());
-  Tool *i = MFI.modify();
-  i->addTool("VT20", "VTOOL", 20, 1000, 0.2, 50);
-  add(MFI);
-
-  dbo::ptr<Tool> MFJ(Wt::cpp14::make_unique<Tool>());
-  Tool *j = MFJ.modify();
-  j->addTool("VT30", "VTOOL", 30, 1000, 0.2, 50);
-  add(MFJ);
-
-  dbo::ptr<Tool> MFK(Wt::cpp14::make_unique<Tool>());
-  Tool *k = MFK.modify();
-  k->addTool("VT35", "VTOOL", 35, 1000, 0.2, 50);
-  add(MFK);
-
-  dbo::ptr<Tool> MFL(Wt::cpp14::make_unique<Tool>());
-  Tool *l = MFL.modify();
-  l->addTool("VT45", "VTOOL", 45, 1000, 0.2, 50);
-  add(MFL);
-
-  dbo::ptr<Tool> MFM(Wt::cpp14::make_unique<Tool>());
-  Tool *m = MFM.modify();
-  m->addTool("VT60", "VTOOL", 60, 1000, 0.2, 50);
-  add(MFM);
-
-  dbo::ptr<Tool> MFN(Wt::cpp14::make_unique<Tool>());
-  Tool *n = MFN.modify();
-  n->addTool("VT88", "VTOOL", 88, 1000, 0.2, 50);
-  add(MFN);
-
-  dbo::ptr<Tool> MFO(Wt::cpp14::make_unique<Tool>());
-  Tool *o = MFO.modify();
-  o->addTool("VT90", "VTOOL", 90, 1000, 0.2, 50);
-  add(MFO);
+  toolDao.insert("AC26", "ACUTE", 26, 1000, 0.2, 50);
+  toolDao.insert("AC30", "ACUTE", 30, 1000, 0.2, 50);
+  toolDao.insert("AC35", "ACUTE", 35, 1000, 0.2, 50);
+  toolDao.insert("AC45", "ACUTE", 45, 1000, 0.2, 50);
+  toolDao.insert("AC60", "ACUTE", 60, 1000, 0.2, 50);
+  toolDao.insert("AC88", "GOOSENECK", 88, 1000, 0.2, 50);
+  toolDao.insert("AC90", "GOOSENECK", 90, 1000, 0.2, 50);
+  toolDao.insert("VT15", "VTOOL", 15, 1000, 0.2, 50);
+  toolDao.insert("VT20", "VTOOL", 20, 1000, 0.2, 50);
+  toolDao.insert("VT30", "VTOOL", 30, 1000, 0.2, 50);
+  toolDao.insert("VT35", "VTOOL", 35, 1000, 0.2, 50);
+  toolDao.insert("VT45", "VTOOL", 45, 1000, 0.2, 50);
+  toolDao.insert("VT60", "VTOOL", 60, 1000, 0.2, 50);
+  toolDao.insert("VT88", "VTOOL", 88, 1000, 0.2, 50);
+  toolDao.insert("VT90", "VTOOL", 90, 1000, 0.2, 50);
 }
 
 dbo::ptr<User> Session::user() const
