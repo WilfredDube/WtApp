8439219c7f1f67e2ab7a19e3993eec64ae3e8944
Wilfred Dube <wilfreddube@gmail.com>
2021-01-24 18:13:31 +0200

    Use Dao objects and clean up

--- view/src/ModelFileWidget.cpp
+++ view/src/ModelFileWidget.cpp
@@ -10,8 +10,15 @@
 #include <Wt/WMessageBox.h>
 #include <Wt/WPushButton.h>
 
-#include "../../libfxtract/include/sheet-metal-component/bend/ModelBend.h"
 #include "../../libfxtract/include/sheet-metal-component/SheetMetal.h"
+#include "../../libfxtract/include/sheet-metal-component/BoostSerializer.h"
+
+#include "../../libfxtract/include/cad-file-reader/CadFileReader.h"
+#include "../../libfxtract/include/cad-file-reader/CadFileReaderFactory.h"
+#include "../../libfxtract/include/cad-file-reader/ReaderCreationFailedException.h"
+
+#include "../../logging/include/LoggingFacility.h"
+#include "../../logging/include/StandardOutputLogger.h"
 
 #include "../../dbdao/include/ModelFileDao.h"
 #include "../../dbdao/include/BendFeatureDao.h"
@@ -19,133 +26,17 @@
 #include "../../dbdao/include/ProcessPlanDao.h"
 #include "../../dbdao/include/BendSequenceDao.h"
 #include "../../dbdao/include/MaterialDao.h"
+#include "../../dbdao/include/FolderStructure.h"
 
 #include "../../libseqgen/include/BendSequenceGenerator.h"
 
-#include <vector>
+#include <algorithm>
 #include <map>
-#include <utility>
-#include <future>
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <filesystem>
+#include <vector>
 
-#include <boost/archive/tmpdir.hpp>
-
-#include <boost/archive/text_iarchive.hpp>
-#include <boost/archive/text_oarchive.hpp>
-
-#include <boost/serialization/base_object.hpp>
-#include <boost/serialization/utility.hpp>
-#include <boost/serialization/map.hpp>
-#include <boost/serialization/assume_abstract.hpp>
 
 using namespace Fxt;
-namespace fs = std::filesystem;
-
-namespace {
-    const Standard_Real kMachineModuleLength = 1000.0;
-    double total_time = 0;
-    std::map<std::string, std::vector<ModelBend>> modelFeatureMap;
-    std::unique_ptr<Fxt::Model> test;
-
-    std::string save(const Fxt::Model &m){
-        std::stringstream ss;
-        boost::archive::text_oarchive oa(ss);
-        oa << m;
-
-        return ss.str();
-    }
-
-    std::unique_ptr<Model> restore(std::string restoreStr)
-    {
-        auto restored = std::make_unique<Model>();
-
-        std::stringstream iss(restoreStr);
-        boost::archive::text_iarchive oa(iss);
-        oa >> *restored;
-
-        return restored;
-    }
-
-    static Model extractFeatures(const char *modelFile)
-    {
-        test = std::make_unique<Model>(modelFile);
-        test->init();
-        test->classifyFaces();
-        test->computeBendAngles();
-
-        if(test->cleanModel())
-            std::cout << "++++++++++++++++CLEANED!!++++++++++++++++" << std::endl;
-        
-        test->printBendInfo();
-
-        std::cout << "extractFeatures------" << std::endl;
-        return *test;
-    }
-
-    static long double procModelFile(const char *modelFile)
-    {
-        std::future<Model> futureData = std::async(std::launch::async, extractFeatures, modelFile);
-
-        auto modelFeatures = futureData.get();
-
-        modelFeatureMap[std::string(modelFile)] = modelFeatures.getBends();
-
-        std::cout << "procModelFile---------" << std::endl;
-
-        return modelFeatures.getThickness();
-    }
-
-    static size_t computerModules(std::vector<ModelBend> bendFeatureData)
-    {
-        Standard_Real longest = 0.0;
-        for (auto bend : bendFeatureData) {
-            if (longest < roundd(bend.getBendLength())) {
-                longest = roundd(bend.getBendLength());
-            }
-        }
-
-        int num_of_modules = 1;
-
-        if (longest > kMachineModuleLength){
-            num_of_modules = longest / kMachineModuleLength;
-
-            if ((static_cast<int>(longest) % static_cast<int>(kMachineModuleLength)) > 0)
-                ++num_of_modules;
-        }
-
-        return num_of_modules;
-    }
-
-}
-
-class ColorWidget : public Wt::WContainerWidget
-{
-public:
-    ColorWidget() : Wt::WContainerWidget()
-    {
-        setMaximumSize(10, 10);
-        setPadding(10);
-        addStyleClass("model-color-red", true);
-    }
-
-    void setIconColor(ProcessLevel pL) 
-    {
-        switch (pL)
-        {
-        case ProcessLevel::FEATURE_EXTRACTED:
-            addStyleClass("model-color-orange", true);
-            break;
-        case ProcessLevel::PROCESS_PLAN_GEN:
-            addStyleClass("model-color-green", true);
-            break;
-        default:
-            break;
-        }
-    }
-};
+using namespace Fxt::Dao;
 
 ModelFileWidget::ModelFileWidget(Session& session, dbo::ptr<ModelFile> model)
     : WContainerWidget(),
@@ -178,11 +69,8 @@ ModelFileWidget::ModelFileWidget(Session& session, dbo::ptr<ModelFile> model)
     });
 }
 
-ModelFileWidget::~ModelFileWidget(){}
-
 void ModelFileWidget::processModelFile()
 {
-    std::vector<ModelBend> bendFeatureData;
     std::string filename = modelFile_->getCadFileDir().toUTF8() + modelFile_->getCadFileName().toUTF8(); 
 
     switch (modelFile_->getProcessLevel())
@@ -192,142 +80,16 @@ void ModelFileWidget::processModelFile()
 
         extractFeatures(modelFile_, session_, filename);
 
-                for (const dbo::ptr<Tool> tool : tools){
-                    toolName = tool->tool_id;
-                    std::cout << "FOUND TOOL : " << toolName << std::endl;
-                    break;
-                }
-
-                dbo::ptr<Material> material = session_.find<Material>()
-                                    .where("material_name = ?").bind(modelFile_->modelMaterial);     
-
-                std::cout << "Material : " << material->material_name
-                          << "K Factor : " << material->k_factor
-                          << "TStrength : " << material->tensile_strength << std::endl;
-                          
-                auto bend_force = a.getBendLength() * std::pow(thickness, 2) 
-                                * material->k_factor 
-                                * material->tensile_strength
-                                / (8 * thickness);
-
-                dbo::ptr<BendFeature> bendFeature(Wt::cpp14::make_unique<BendFeature>());
-                BendFeature *b = bendFeature.modify();
-                
-                session_.add(bendFeature);
-
-                b->bend_id = a.getFaceId();
-                b->face_id1 = a.getJoiningFaceID1();
-                b->face_id2 = a.getJoiningFaceID2();
-                b->bend_angle = a.getBendAngle();
-                b->bend_radius = a.getBendRadius();
-                b->bend_length = a.getBendLength();
-                b->modelFile = modelFile_;
-                b->bend_direction = 0;
-                b->bending_tool_id = toolName;
-                b->bend_force = bend_force;
-
-                modelFile_.modify()->thickness = thickness;
-                modelFile_.modify()->bendingForce = bend_force;
-
-                session_.flush();
-
-                t.commit();
-            }
-
-            dbo::Transaction t(session_);
-            
-            modelFile_.modify()->processLevel = ProcessLevel::FEATURE_EXTRACTED;
-            modelFile_.modify()->nbends = bendFeatureData.size();
-            modelFile_.modify()->nModules = computerModules(bendFeatureData);
-            modelFile_.modify()->modelData = save(*test);
-            modelFile_.modify()->feature_recognition_time = total_time;
-
-            icons_->setIconColor(ProcessLevel::FEATURE_EXTRACTED);
-
-            t.commit();
-        }
-
-        // std::cout << "\n\n\n" << std::endl;
+        icons_->setIconColor(ProcessLevel::FEATURE_EXTRACTED);
 
         session_.modelFeaturesExtracted().emit(modelFile_);
-        std::cout << "Done......................." << std::endl;
         break;
     case ProcessLevel::FEATURE_EXTRACTED:
-        total_time = 0;
         std::cout << "Process Planning started..." << std::endl;        
-        {
-            std::string restoreStr;
-            std::vector<int> bids; 
-            
-            dbo::Transaction t(session_);
-
-            restoreStr = modelFile_.modify()->getModelData().toUTF8();
-
-            auto& bendFeatures = modelFile_.modify()->bendFeatures;
-            
-            auto restored = restore(restoreStr);
-
-            std::vector<size_t> bDirs;
-            for(auto& bend : bendFeatures) {
-                restored->getBendMap().find(bend->bend_id)->second.setBendDirection(bend->bend_direction);
-            }
-
-            modelFile_.modify()->modelData = save(*restored);
-            restoreStr = modelFile_.modify()->modelData.toUTF8();
-            restored = restore(restoreStr);
-
-            for (const auto& bend : restored->getBendMap()) {
-                bids.push_back(static_cast<int>(bend.first));
-            }
-
-            std::cout << ">>>>>>>>>> Filename : " << restored->getModelFile() << std::endl;
-
-            int startTime = clock();
-            Sequence bestSequence = generateBendingSeq(*restored, bids);
-            int stopTime = clock();
-
-            total_time = (stopTime - startTime) / double(CLOCKS_PER_SEC);
-            // std::cout << ">>>>>>>>>> Bend sequence time : " << total_time <<std::endl;
-            // std::cout << total_time << std::endl;
 
         generateBendingSequence(modelFile_, session_);
 
-            session_.add(processPlan);
-
-            pp->dateCreated = Wt::WDateTime::currentDateTime();
-            pp->no_flips = bestSequence.nFlips;
-            pp->no_rotations = bestSequence.nRotations;
-            pp->process_planning_time += total_time + modelFile_.modify()->feature_recognition_time;
-            pp->modelFile = modelFile_;
-            pp->quantity = 1;
-            pp->tool_distance = bestSequence.distance;
-
-            for(const auto& bs : bestSequence.chromosome )
-            {
-                dbo::ptr<BendSequence> bendSeq = session_.add(Wt::cpp14::make_unique<BendSequence>());
-              
-                bendSeq.modify()->bend_id = bs;
-                bendSeq.modify()->processPlan = processPlan;
-            }
-
-            dbo::ptr<MachineParam> machineParam(Wt::cpp14::make_unique<MachineParam>());
-            MachineParam *mp = machineParam.modify();
-
-            session_.add(machineParam);
-            mp->num_tools = bestSequence.nTools;
-            mp->processPlan = processPlan;
-
-            modelFile_.modify()->processLevel = ProcessLevel::PROCESS_PLAN_GEN;
-
-            session_.flush();            
-
-            t.commit(); 
-        }
-
-        std::cout << "\n" << std::endl;
-        
         icons_->setIconColor(ProcessLevel::PROCESS_PLAN_GEN );
-        // session_.modelFeaturesExtracted().emit(modelFile_);
         break;
     case ProcessLevel::PROCESS_PLAN_GEN:
 
@@ -352,13 +114,19 @@ void ModelFileWidget::deleteModelFile()
                                 );
     if (answer == Wt::StandardButton::Ok) {
         rm();
+        session_.modelFileDeleted().emit(modelFile_);
     }
 }
 
 void ModelFileWidget::rm()
 {
-    dbo::Transaction t(session_);
+    deleteFolder(modelFile_->getCadFileDir().toUTF8());
 
+    ModelFileDao modelFileDao { session_ };
+    modelFileDao.deleteProject(modelFile_);
+    
+    this->removeFromParent();
+}
 
 void ModelFileWidget::extractFeatures(Wt::Dbo::ptr<ModelFile>& modelFile, Session& session, std::string filename)
 {
