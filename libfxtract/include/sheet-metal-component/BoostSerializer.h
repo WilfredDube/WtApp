f6adb4ecb86f10058e378b209902fa2533adf32e
Wilfred Dube <wilfreddube@gmail.com>
2021-01-24 18:20:28 +0200

    Implement serialization and de-serialization methods

--- libfxtract/include/sheet-metal-component/BoostSerializer.h
+++ libfxtract/include/sheet-metal-component/BoostSerializer.h
@@ -9,3 +9,25 @@
 #include <boost/serialization/utility.hpp>
 #include <boost/serialization/map.hpp>
 #include <boost/serialization/assume_abstract.hpp>
+
+#include "SheetMetal.h"
+
+std::string save(const std::shared_ptr<Fxt::SheetMetalComponent::SheetMetal>& sheetMetalFeatureModel)
+{
+    std::stringstream ss;
+    boost::archive::text_oarchive oa(ss);
+    oa << sheetMetalFeatureModel;
+
+    return ss.str();
+}
+
+std::shared_ptr<Fxt::SheetMetalComponent::SheetMetal> restore(std::string restoreStr)
+{
+    auto restored = std::make_unique<Fxt::SheetMetalComponent::SheetMetal>();
+
+    std::stringstream iss(restoreStr);
+    boost::archive::text_iarchive oa(iss);
+    oa >> *restored;
+
+    return restored;
+}
